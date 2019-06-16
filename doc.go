b9c9f580bf04859944d4b8e444f70985ff3e92e3
Fouad Matin <m@fouad.org>
2019-06-16 15:00:39 -0700

    fix(doc): index

--- doc.go
+++ doc.go
@@ -1,9 +1,9 @@
-package apis
-
 // Package apis is the monorepo for Indent Go packages for managing access control reliably across systems.
 //
-// Want to get started? Learn more at https://indent.com/docs →
+// Want to get started?
+// Learn more at https://indent.com/docs →
 //
 // Installation
 //
 //  go get -u go.indent.com/apis
+package apis
